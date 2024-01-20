87f3969d57ed738c01ce2d4739f4e263449aa00a
Halim <lamanele23@gmail.com>
2024-01-20 20:58:11 +0700

    add Jadwal praktek

--- src/model/JadwalPraktekModel.java
+++ src/model/JadwalPraktekModel.java
@@ -8,41 +8,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JadwalPraktekModel {
-    private static List<JadwalPraktek> listJadwalPraktek;
-    private ModelGSON modelGSON;
+    private static ArrayList<JadwalPraktek> listJadwalPraktek;
+    private final ModelGSON modelGSON;
     public JadwalPraktekModel(){
         modelGSON = new ModelGSON("src/DB/jadwalPraktek.json");
         listJadwalPraktek = modelGSON.readFromFile(new TypeToken<ArrayList<JadwalPraktek>>() {}.getType());
         if (listJadwalPraktek == null) listJadwalPraktek = new ArrayList<>();
     }
 
-    public static void init(){
+    public void init(){
         tambahJadwalPraktek("Dr. Tata","Senin", "09:00 - 12:00");
         tambahJadwalPraktek("Dr. Tita","Selasa", "09:00 - 12:00");
         tambahJadwalPraktek("Dr. Totot","Sabtu", "09:00 - 12:00");
         tambahJadwalPraktek("Dr. Tati","Minggu", "09:00 - 12:00");
         tambahJadwalPraktek("Dr. Tra","Rabu", "09:00 - 12:00");
     }
-    public static void tambahJadwalPraktek(String namaDokter, String hari, String jamPraktek) {
+    public void tambahJadwalPraktek(String namaDokter, String hari, String jamPraktek) {
         JadwalPraktek jadwalPraktek = new JadwalPraktek(namaDokter, hari, jamPraktek);
         listJadwalPraktek.add(jadwalPraktek);
+        modelGSON.writeToFile(listJadwalPraktek);
     }
 
-    public static List<JadwalPraktek> getAllJadwalPraktek() {
+    public ArrayList<JadwalPraktek> getAllJadwalPraktek() {
         return listJadwalPraktek;
     }
 
-    public static void updateJadwalPraktek(int index, String namaDokter, String hari, String jamPraktek) {
+    public void updateJadwalPraktek(int index, String namaDokter, String hari, String jamPraktek) {
         if (index >= 0 && index < listJadwalPraktek.size()) {
             JadwalPraktek jadwalPraktek = listJadwalPraktek.get(index);
             jadwalPraktek.setNamaDokter(namaDokter);
             jadwalPraktek.setHari(hari);
             jadwalPraktek.setJamPraktek(jamPraktek);
         }
+        modelGSON.writeToFile(listJadwalPraktek);
     }
 
     public static int getLastId() {
-        if (listJadwalPraktek.isEmpty()) return 0;
+        if (listJadwalPraktek == null) return 0;
         return listJadwalPraktek.size();
     }
 }
