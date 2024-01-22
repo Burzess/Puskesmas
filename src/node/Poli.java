c21e29089cbd0c4b18ecd5925d6bf937fa126d5e
Halim <lamanele23@gmail.com>
2024-01-22 17:35:00 +0700

    +

--- src/node/Poli.java
+++ src/node/Poli.java
@@ -6,23 +6,14 @@ public class Poli {
     public int idPoli;
     public String namaPoli;
     public ArrayList<Dokter> dokters;
-    public ArrayList<Antrian> antrians = new ArrayList<>();
+    public ArrayList<Antrian> antrians;
 
     public Poli(int idPoli, String namaPoli) {
         this.idPoli = idPoli;
         this.namaPoli = namaPoli;
-        this.dokters = new ArrayList<>();
         this.antrians = new ArrayList<>();
     }
-
-    public void addDokter(Dokter dokter){
-        this.dokters.add(dokter);
-    }
-
     public void addAntrian(Antrian antrian) {
         this.antrians.add(antrian);
-    }
-    public void deleteAntrian(Antrian antrian){
-        this.antrians.remove(antrian);
     }
 }
